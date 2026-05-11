37a75754534ed31907b7c53a6f31ea982e22a517
Ha DaSom <2371068@ewhain.net>
2026-05-11 15:18:26 +0900

    fix: application status passStatus 변경

--- src/db_project2026_team03/dto/ApplicationDTO.java
+++ src/db_project2026_team03/dto/ApplicationDTO.java
@@ -5,17 +5,17 @@ public class ApplicationDTO {
     private int recruitmentId;
     private String studentId;
     private String selfIntro;
-    private String status;
+    private String passStatus;
 
     public ApplicationDTO() {}
 
     public ApplicationDTO(int applicationId, int recruitmentId, String studentId, 
-                          String selfIntro, String status) {
+                          String selfIntro, String passStatus) {
         this.applicationId = applicationId;
         this.recruitmentId = recruitmentId;
         this.studentId = studentId;
         this.selfIntro = selfIntro;
-        this.status = status;
+        this.passStatus = passStatus;
     }
 
     public int getApplicationId() {return applicationId;}
@@ -26,6 +26,6 @@ public class ApplicationDTO {
     public void setStudentId(String studentId) {this.studentId = studentId;}
     public String getSelfIntro() {return selfIntro;}
     public void setSelfIntro(String selfIntro) {this.selfIntro = selfIntro;}
-    public String getStatus() {return status;}
-    public void setStatus(String status) {this.status = status;}
+    public String getPassStatus() {return passStatus;}
+    public void setPassStatus(String passStatus) {this.passStatus = passStatus;}
 }

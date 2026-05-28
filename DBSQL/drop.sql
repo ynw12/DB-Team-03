6a6dd31fdd621b2fb9e44e00edd900fb86ab2440
Yeonwoo Lee <yeonwoo845@gmail.com>
2026-05-28 09:58:38 +0900

    feat:DROP VIEW

--- DBSQL/drop.sql
+++ DBSQL/drop.sql
@@ -1,18 +1,24 @@
--- 1. 가장 하위에 있는 자식 테이블
+-- 1. DROP VIEW
+DROP VIEW IF EXISTS vw_all_recruitments;
+DROP VIEW IF EXISTS vw_active_recruitments;
+DROP VIEW IF EXISTS vw_student_bookmark;
+DROP VIEW IF EXISTS vw_student_scrap;
+
+-- 2. 가장 하위에 있는 자식 테이블
 DROP TABLE IF EXISTS Application;
 DROP TABLE IF EXISTS Scrap;
 DROP TABLE IF EXISTS Bookmark;
 
--- 2. 중간 단계 자식 테이블
+-- 3. 중간 단계 자식 테이블
 DROP TABLE IF EXISTS Recruitment;
 
--- 3. 핵심 테이블
+-- 4. 핵심 테이블
 DROP TABLE IF EXISTS Organization;
 
--- 4. 최상위 부모 테이블
+-- 5. 최상위 부모 테이블
 DROP TABLE IF EXISTS Student;
 DROP TABLE IF EXISTS Category;
 DROP TABLE IF EXISTS OrganizationType;
 
--- 5. DROP DATABASE
-DROP DATABASE DB2026team03;
+-- 6. DROP DATABASE
+DROP DATABASE DB2026Team03;
